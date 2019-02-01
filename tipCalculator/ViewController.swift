a576989bf9433d23a20725091706497800ccdc08
Giovanni Rosas-Marin <marin@giovannis-macbook-pro.local>
2019-02-01 00:22:47 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Giovanni Rosas-Marin on 2/1/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
